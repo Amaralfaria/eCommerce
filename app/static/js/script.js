9a5b5472b7b14e6020f29e728bdf5a4872593651
Lucas Amaral <lucasamaralfaria@gmail.com>
2023-12-11 14:07:25 -0300

    alteracoes index

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,64 +1,66 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetchProdutos();
+    obterProdutos()
 });
 
-function cadastrarUsuario(){
-    var username = document.getElementById("username").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var is_fornecedor = document.getElementById("is_fornecedor").value;
-    var telefone = document.getElementById("telefone").value;
+// function fetchProdutos() {
+//     fetch('http://localhost:8000/produtos/')
+//         .then(response => response.json())
+//         .then(data => displayProdutos(data.produtos))
+//         .catch(error => console.error('Erro ao buscar produtos:', error));
+// }
 
-    // Monta os dados do novo usuário
-    var novoUsuario = {
-        "username": username,
-        "email": email,
-        "password": password,
-        "telefone": telefone,
-        "is_fornecedor": is_fornecedor,
-        "is_cliente": !is_fornecedor
-    };
+// function displayProdutos(produtos) {
+//     const container = document.getElementById('lista-produtos');
+//     container.innerHTML = ''; // Limpa a lista atual
+//     produtos.forEach(produto => {
+//         const div = document.createElement('div');
+//         div.innerHTML = `<h3>${produto.nome}</h3><p>R$ ${produto.preco}</p>`;
+//         container.appendChild(div);
+//     });
+// }
+
+// function pesquisarProdutos() {
+//     let termo = document.getElementById('barra-pesquisa').value;
+//     fetch(`http://localhost:8000/produtos/?nomeProduto=${termo}`) // Adapte para a sua URL de pesquisa
+//         .then(response => response.json())
+//         .then(data => displayProdutos(data.produtos))
+//         .catch(error => console.error('Erro ao pesquisar produtos:', error));
+// }
 
 
-    fetch('http://localhost:8000/usuarios/',{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "accept": "application/json"
-        },
-        body: JSON.stringify(novoUsuario)
-    })
-        .then(response => {
-            if (response.status === 201) {
-                console.log(201)
-                window.location.href = 'http://localhost:8000/';
-            } else {
-                throw new Error('Erro na requisição: ' + response.status);
-            }
-        })
-}
-
-function fetchProdutos() {
+function obterProdutos() {
+    // Substitua pela lógica de chamada da API para obter produtos
     fetch('http://localhost:8000/produtos/')
         .then(response => response.json())
-        .then(data => displayProdutos(data.produtos))
-        .catch(error => console.error('Erro ao buscar produtos:', error));
+        .then(produtos => {
+            // Chama a função para exibir os produtos
+            exibirProdutos(produtos.produtos);
+        })
+        .catch(error => {
+            console.error('Erro ao obter produtos:', error);
+        });
 }
 
-function displayProdutos(produtos) {
-    const container = document.getElementById('lista-produtos');
-    container.innerHTML = ''; // Limpa a lista atual
+// Exibe os produtos na lista
+function exibirProdutos(produtos) {
+    var productContainer = document.getElementById('productContainer');
+
     produtos.forEach(produto => {
-        const div = document.createElement('div');
-        div.innerHTML = `<h3>${produto.nome}</h3><p>R$ ${produto.preco}</p>`;
-        container.appendChild(div);
-    });
-}
+        var productCard = document.createElement('div');
+        productCard.classList.add('product-card');
 
-function pesquisarProdutos() {
-    let termo = document.getElementById('barra-pesquisa').value;
-    fetch(`http://localhost:8000/produtos/?nomeProduto=${termo}`) // Adapte para a sua URL de pesquisa
-        .then(response => response.json())
-        .then(data => displayProdutos(data.produtos))
-        .catch(error => console.error('Erro ao pesquisar produtos:', error));
+        var productName = document.createElement('h2');
+        productName.textContent = produto.nome; // Substitua pelo campo correto
+
+        // Adicione a imagem do produto, se aplicável
+        if (produto.imagem) {
+            var productImage = document.createElement('img');
+            productImage.src = produto.imagem; // Substitua pelo campo correto
+            productImage.alt = produto.nome; // Substitua pelo campo correto
+            productCard.appendChild(productImage);
+        }
+
+        productCard.appendChild(productName);
+        productContainer.appendChild(productCard);
+    });
 }
