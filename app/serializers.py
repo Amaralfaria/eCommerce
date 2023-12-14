6939f8c0eab9ff9269ed56df6e50467456982cf5
MatThHeuss <matheusalencar99@gmail.com>
2023-12-14 20:10:34 -0300

    apply lint

--- app/serializers.py
+++ app/serializers.py
@@ -1,85 +1,126 @@
 from rest_framework import serializers
+
 from .models import *
 
 
 class FornecedorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fornecedor
-        fields = ['id','nome_do_negocio', 'endereco', 'latitude', 'longitude','fornecedor_user', 'feira']
-        read_only_fields = ['id']
+        fields = [
+            "id",
+            "nome_do_negocio",
+            "endereco",
+            "latitude",
+            "longitude",
+            "fornecedor_user",
+            "feira",
+        ]
+        read_only_fields = ["id"]
+
 
 class FeiraSerializer(serializers.ModelSerializer):
     class Meta:
         model = Feira
-        fields = '__all__'
-        read_only_fields = ['id']
+        fields = "__all__"
+        read_only_fields = ["id"]
+
 
 class UsuarioSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
     class Meta:
         model = Usuario
-        fields = ['id','username', 'email', 'password',
-                  'telefone','is_cliente','is_fornecedor']
-        read_only_fields = ['id']
-
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "telefone",
+            "is_cliente",
+            "is_fornecedor",
+        ]
+        read_only_fields = ["id"]
 
     def create(self, validated_data):
         user = Usuario.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            telefone=validated_data['telefone'],
-            is_cliente=validated_data['is_cliente'],
-            is_fornecedor=validated_data['is_fornecedor'],
+            username=validated_data["username"],
+            email=validated_data["email"],
+            telefone=validated_data["telefone"],
+            is_cliente=validated_data["is_cliente"],
+            is_fornecedor=validated_data["is_fornecedor"],
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data["password"])
         user.save()
         return user
+
 
 class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
-        fields = ['id','preferencias_de_busca','cliente_user']
-        read_only_fields = ['id']
+        fields = ["id", "preferencias_de_busca", "cliente_user"]
+        read_only_fields = ["id"]
+
 
 class MensagemSerializer(serializers.ModelSerializer):
-    destinatario_id = serializers.IntegerField(write_only=True, label='ID do Destinatário')
-    remetente = serializers.HiddenField(default=serializers.CurrentUserDefault(), write_only=True, label='ID do Remetente')
+    destinatario_id = serializers.IntegerField(
+        write_only=True, label="ID do Destinatário"
+    )
+    remetente = serializers.HiddenField(
+        default=serializers.CurrentUserDefault(),
+        write_only=True,
+        label="ID do Remetente",
+    )
 
-    destinatario_username = serializers.ReadOnlyField(source='destinatario.username', read_only=True)
-    remetente_username = serializers.ReadOnlyField(source='remetente.username', read_only=True)
+    destinatario_username = serializers.ReadOnlyField(
+        source="destinatario.username", read_only=True
+    )
+    remetente_username = serializers.ReadOnlyField(
+        source="remetente.username", read_only=True
+    )
 
     class Meta:
         model = Mensagem
-        fields = ['id', 'destinatario_id', 'remetente', 'conteudo', 'data_envio', 'destinatario_username', 'remetente_username']
+        fields = [
+            "id",
+            "destinatario_id",
+            "remetente",
+            "conteudo",
+            "data_envio",
+            "destinatario_username",
+            "remetente_username",
+        ]
 
     # class Meta:
     #     model = Mensagem
     #     fields = ['id', 'remetente', 'destinatario', 'conteudo', 'data_envio']
-        
+
 
 class ProdutoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Produto
-        fields = ['id','nome', 'descricao', 'preco', 'categoria', 'fornecedor']
-        read_only_fields = ['id']
+        fields = ["id", "nome", "descricao", "preco", "categoria", "fornecedor"]
+        read_only_fields = ["id"]
+
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
-        fields = '__all__'
-        read_only_fields = ['id']
+        fields = "__all__"
+        read_only_fields = ["id"]
+
 
 class CompraSerializer(serializers.ModelSerializer):
     class Meta:
         model = Compra
-        fields = '__all__'
-        read_only_fields = ['id']
+        fields = "__all__"
+        read_only_fields = ["id"]
+
 
 class AvaliacaoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Avaliacao
-        fields = ['id','cliente', 'produto', 'nota', 'comentario']
-        read_only_fields = ['id']
+        fields = ["id", "cliente", "produto", "nota", "comentario"]
+        read_only_fields = ["id"]
 
 
 # class RelatorioSerializer(serializers.ModelSerializer):
@@ -93,4 +134,3 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 #         model = Usuario
 #         fields = ['email', 'password']
 #         read_only_fields = ['email','password']
-    
